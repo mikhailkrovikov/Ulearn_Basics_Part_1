5cb3369f20dbcfd8b320b69ded54ab979094496f
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-06-29 11:49:38 +0300

    dragon fractal task

---
+++ Module_3_Cycles/Fractals/DragonFractalTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace Fractals
+{
+    internal static class DragonFractalTask
+    {
+        public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+        {
+            double x = 1;
+            double y = 0;
+            double x1, y1;
+            var random = new Random(seed);
+
+            for (int i = 0; i < iterationsCount; i++)
+            {
+                if (random.Next(2) == 0)
+                {
+                    x1 = (x * Math.Cos(Math.PI / 4) - y * Math.Sin(Math.PI / 4)) / Math.Sqrt(2);
+                    y1 = (x * Math.Sin(Math.PI / 4) + y * Math.Cos(Math.PI / 4)) / Math.Sqrt(2);
+                }
+                else
+                {
+                    x1 = (x * Math.Cos(3 * Math.PI / 4) - y * Math.Sin(3 * Math.PI / 4)) / Math.Sqrt(2) + 1;
+                    y1 = (x * Math.Sin(3 * Math.PI / 4) + y * Math.Cos(3 * Math.PI / 4)) / Math.Sqrt(2);
+                }
+
+                x = x1;
+                y = y1;
+
+                pixels.SetPixel(x, y);
+            }
+        }
+    }
+}
