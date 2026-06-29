a2e583b0f2dd27bd325e1d11e1175d120792c1ad
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-06-29 14:24:22 +0300

    triangle task

--- Module_10_Practice/manipulator/TriangleTask.cs
+++ Module_10_Practice/manipulator/TriangleTask.cs
@@ -5,12 +5,13 @@ namespace Manipulation;
 
 public class TriangleTask
 {
-    /// <summary>
-    /// Возвращает угол (в радианах) между сторонами a и b в треугольнике со сторонами a, b, c 
-    /// </summary>
     public static double GetABAngle(double a, double b, double c)
     {
-        return 0.0;
+        if ((a + b < c) || (b + c < a) || (a + c < b))
+            return double.NaN;
+        if (c <= 0 && (a > 0 || b > 0))
+            return 0;
+        return Math.Acos((a * a + b * b - c * c) / (2 * a * b));
     }
 }
 
@@ -19,9 +20,25 @@ public class TriangleTask_Tests
 {
     [TestCase(3, 4, 5, Math.PI / 2)]
     [TestCase(1, 1, 1, Math.PI / 3)]
-    // добавьте ещё тестовых случаев!
+    [TestCase(150, 120, 60, 0.3897607327974747)]
+    [TestCase(60, 120, 150, 1.8886200307227774)]
+    [TestCase(1, 1, 2, Math.PI)]
+    [TestCase(2, 1, 1, 0)]
+    [TestCase(1, 2, 1, 0)]
+    [TestCase(1, 1, 2.001, double.NaN)]
+    [TestCase(1, 2.001, 1, double.NaN)]
+    [TestCase(2.001, 1, 1, double.NaN)]
+    [TestCase(0, 5, 5, double.NaN)]
+    [TestCase(5, 0, 5, double.NaN)]
+    [TestCase(5, 5, 0, 0)]
+    [TestCase(-3, -2, -4, double.NaN)]
+    [TestCase(1, 1, -1, double.NaN)]
     public void TestGetABAngle(double a, double b, double c, double expectedAngle)
     {
-        Assert.Fail("Not implemented yet");
+        NUnit.Framework.Legacy.ClassicAssert.AreEqual(
+            expectedAngle,
+            TriangleTask.GetABAngle(a, b, c),
+            1e-9,
+            "Angle Test");
     }
 }
