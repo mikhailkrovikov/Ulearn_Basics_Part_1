a681d3eeb02b667a8de9f85de3876323fddbdf33
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-06-29 11:47:02 +0300

    mazes emptymaze task

---
+++ Module_3_Cycles/Mazes/EmptyMazeTask.cs
@@ -0,0 +1,27 @@
+﻿namespace Mazes
+{
+    public static class EmptyMazeTask
+    {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            MoveRight(robot, width);
+            MoveDown(robot, height);
+        }
+
+        public static void MoveRight(Robot robot, int width)
+        {
+            while (robot.X < width - 2)
+            {
+                robot.MoveTo(Direction.Right);
+            }
+        }
+
+        public static void MoveDown(Robot robot, int hight)
+        {
+            while (robot.Y < hight - 2)
+            {
+                robot.MoveTo(Direction.Down);
+            }
+        }
+    }
+}
