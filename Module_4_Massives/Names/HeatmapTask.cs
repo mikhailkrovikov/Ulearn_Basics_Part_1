5bcce38bfff9247b24daaed9978ddd2bf8a55d66
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-06-29 11:55:20 +0300

    heatmap data task

--- Module_4_Massives/Names/HeatmapTask.cs
+++ Module_4_Massives/Names/HeatmapTask.cs
@@ -1,13 +1,33 @@
-﻿namespace Names;
+﻿using System.Xml.Linq;
+
+namespace Names;
 
 internal static class HeatmapTask
 {
     public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
     {
-        return new HeatmapData(
-            "Пример карты интенсивностей",
-            new double[,] { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 4 }, { 4, 4, 4 } }, 
-            new[] { "a", "b", "c", "d" }, 
-            new[] { "X", "Y", "Z" });
+        string[] days = new string[30];
+        string[] months = new string[12];
+        double[,] mapOfDay = new double[30, 12];
+
+        for (int i = 0; i < days.Length; i++)
+        {
+            days[i] = (i + 2).ToString();
+        }
+
+        for (int i = 0; i < months.Length; i++)
+        {
+            months[i] = (i + 1).ToString();
+        }
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (names[i].BirthDate.Day == 1) continue;
+            int indexOfDay = names[i].BirthDate.Day - 2;
+            int indexOfMonth = names[i].BirthDate.Month - 1;
+            mapOfDay[indexOfDay, indexOfMonth]++;
+        }
+
+        return new HeatmapData("Пример карты интенсивностей", mapOfDay, days, months);
     }
 }
