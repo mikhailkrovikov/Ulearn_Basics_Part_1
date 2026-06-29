2265746c8fa9cc11adcb6814d0cb7367a2ced72d
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-06-29 11:47:27 +0300

    snakemaze task

--- Module_3_Cycles/Mazes/SnakeMazeTask.cs
+++ Module_3_Cycles/Mazes/SnakeMazeTask.cs
@@ -1,9 +1,54 @@
-﻿namespace Mazes;
-
-public static class SnakeMazeTask
+﻿namespace Mazes
 {
-    public static void MoveOut(Robot robot, int width, int height)
+    public static class SnakeMazeTask
     {
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            MoveRight(robot, width);
+            while (robot.Y != height - 2)
+            {
+                if (robot.X == width - 2)
+                    DownAndLeft(robot, width);
+                else
+                    DownAndRight(robot, width);
+            }
+        }
 
+        public static void MoveRight(Robot robot, int width)
+        {
+            while (robot.X < width - 2)
+            {
+                robot.MoveTo(Direction.Right);
+            }
+        }
+
+        public static void MoveDown(Robot robot, int hight)
+        {
+            robot.MoveTo(Direction.Down);
+            robot.MoveTo(Direction.Down);
+        }
+
+        public static void MoveLeft(Robot robot, int width)
+        {
+            if (robot.X == width - 2)
+                while (robot.X > 1)
+                {
+                    robot.MoveTo(Direction.Left);
+                }
+        }
+
+        public static void DownAndLeft(Robot robot, int width)
+        {
+            robot.MoveTo(Direction.Down);
+            robot.MoveTo(Direction.Down);
+            MoveLeft(robot, width);
+        }
+
+        public static void DownAndRight(Robot robot, int width)
+        {
+            robot.MoveTo(Direction.Down);
+            robot.MoveTo(Direction.Down);
+            MoveRight(robot, width);
+        }
     }
 }
