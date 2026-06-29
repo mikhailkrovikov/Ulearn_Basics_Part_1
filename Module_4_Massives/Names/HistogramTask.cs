16dbf67234440a430ca71c9e126a5f79764b6cbe
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-06-29 11:54:50 +0300

    histogram data task

---
+++ Module_4_Massives/Names/HistogramTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Names;
+internal static class HistogramTask
+{
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        double[] frequency = new double[31];
+        foreach (var element in names)
+        {
+            if (element.Name == name && element.BirthDate.Day != 1)
+            {
+                int data = element.BirthDate.Day;
+                frequency[data - 1]++;
+            }
+        }
+        string[] days = new string[31];
+        for (int i = 0; i < days.Length; i++)
+        {
+            days[i] = (i + 1).ToString();
+        }
+
+        return new HistogramData
+            (string.Format("Рождаемость людей с именем '{0}'", name),
+            days,
+            frequency);
+    }
+}
