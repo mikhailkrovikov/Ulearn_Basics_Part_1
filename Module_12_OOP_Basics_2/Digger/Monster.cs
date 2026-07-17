b0c8c3102b7f51a54f3a9c3770ba79b6fbb2418f
mkrovikov <mikhailkrovikov@gmail.com>
2026-07-17 23:17:02 +0300

    monster task

--- Module_12_OOP_Basics_2/Digger/Monster.cs
+++ Module_12_OOP_Basics_2/Digger/Monster.cs
@@ -1,27 +1,62 @@
 ﻿using Digger.Architecture;
+using System;
 
 namespace Digger
 {
+    class SpecialCommands
+    {
+        public static readonly CreatureCommand EmptyMove = new();
+    }
     public class Monster : ICreature
     {
         public CreatureCommand Act(int x, int y)
         {
-            return new CreatureCommand();
+            var (isPlayerAlive, playerX, playerY) = FindPlayerIfAlive();
+            if (!isPlayerAlive)
+                return SpecialCommands.EmptyMove;
+            var result = new CreatureCommand();
+            if (playerX == x)
+                result.DeltaY = Math.Sign(playerY - y);
+            else
+                result.DeltaX = Math.Sign(playerX - x);
+            if (CanMove(x + result.DeltaX, y + result.DeltaY))
+                return result;
+            return SpecialCommands.EmptyMove;
         }
 
         public bool DeadInConflict(ICreature conflictedObject)
         {
-            return false;
+            return (conflictedObject is Sack) || (conflictedObject is Monster);
         }
 
         public int GetDrawingPriority()
         {
-            return 5;
+            return -1;
         }
 
         public string GetImageFileName()
         {
             return "Monster.png";
+        }
+
+        private static (bool, int, int) FindPlayerIfAlive()
+        {
+            for (int i = 0; i < Game.MapWidth; i++)
+                for (int j = 0; j < Game.MapHeight; j++)
+                {
+                    var cell = Game.Map[i, j];
+                    if (cell is Player)
+                        return (true, i, j);
+                }
+            return (false, 0, 0);
+        }
+
+        private static bool CanMove(int x, int y)
+        {
+            var destinationCell = Game.Map[x, y];
+            return (destinationCell is null)
+                || (destinationCell is Gold)
+                || (destinationCell is Player);
         }
     }
 }
