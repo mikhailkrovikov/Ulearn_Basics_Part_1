143f045fc21701fa6484f4fc393ebe16329da30a
mkrovikov <mikhailkrovikov@gmail.com>
2026-07-22 22:19:08 +0300

    threshold task

--- Module_7_Complexity_Of_Algorithms/image/ThresholdFilterTask.cs
+++ Module_7_Complexity_Of_Algorithms/image/ThresholdFilterTask.cs
@@ -1,9 +1,27 @@
-﻿namespace Recognizer;
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Recognizer;
 
 public static class ThresholdFilterTask
 {
-	public static double[,] ThresholdFilter(double[,] original, double whitePixelsFraction)
-	{
-		return new double[original.GetLength(0), original.GetLength(1)];
-	}
+    public static double[,] ThresholdFilter(double[,] original, double whitePixelsFraction)
+    {
+        var length = original.GetLength(0);
+        var height = original.GetLength(1);
+        var t = (int)(original.Length * whitePixelsFraction);
+        var list = new List<double>();
+        for (int i = 0; i <= length - 1; i++)
+            for (int j = 0; j <= height - 1; j++)        
+                list.Add(original[i, j]);
+              
+        list = list.OrderByDescending(x => x).Take(t).ToList();
+        for(int i = 0; i <= length - 1; i++)
+            for (int j = 0; j <= height - 1; j++)            
+                if (list.Contains(original[i, j]))            
+                    original[i, j] = 1;        
+                else               
+                    original[i, j] = 0;                 
+        return original;
+    }
 }
