e0274cc8d7eea32a2b6b778d88fa72725a3a2cd0
mkrovikov <mikhailkrovikov@gmail.com>
2026-07-22 21:52:14 +0300

    grayscale task

---
+++ Module_7_Complexity_Of_Algorithms/image/ThresholdFilterTask.cs
@@ -0,0 +1,9 @@
+﻿namespace Recognizer;
+
+public static class ThresholdFilterTask
+{
+	public static double[,] ThresholdFilter(double[,] original, double whitePixelsFraction)
+	{
+		return new double[original.GetLength(0), original.GetLength(1)];
+	}
+}
